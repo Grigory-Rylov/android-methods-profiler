e7838a7e71113762a6c244c676656b5cda884d71
Roman Tikhonov <ggys@yandex-team.ru>
2022-04-15 12:08:41 +0300

    COMPARE-TRACES: impl compare flame chart algorithm

---
+++ core/src/main/java/com/github/grishberg/profiler/comparator/AggregatedFlameChartComparator.kt
@@ -0,0 +1,40 @@
+package com.github.grishberg.profiler.comparator
+
+class AggregatedFlameChartComparator {
+
+    fun compare(referenceNode: AggregatedFlameProfileData, testedNode: AggregatedFlameProfileData) {
+        val (ref, test) = compareChildren(referenceNode.children, testedNode.children)
+
+        for ((name, refData) in ref) {
+            val testedData = test[name] ?: continue  // Should be handled
+            compare(refData, testedData)
+        }
+    }
+
+    private fun compareChildren(
+        refChildren: List<AggregatedFlameProfileData>,
+        testedChildren: List<AggregatedFlameProfileData>
+    ): Pair<Map<String, AggregatedFlameProfileData>, Map<String, AggregatedFlameProfileData>> {
+        val reference = refChildren.associateBy { it.name }
+        val tested = testedChildren.associateBy { it.name }
+
+        for ((methodName, refData) in reference) {
+            val testedData = tested[methodName]
+            if (testedData == null) {
+                refData.mark = FlameMarkType.OLD_OLD
+            } else if (refData.mean - testedData.mean > 1) {
+                refData.mark = FlameMarkType.MAYBE_OLD
+            } else if (testedData.mean - refData.mean > 1) {
+                testedData.mark = FlameMarkType.MAYBE_NEW
+            }
+        }
+        for ((testedMethodName, testedData) in tested) {
+            val refData = reference[testedMethodName]
+            if (refData == null) {
+                testedData.mark = FlameMarkType.NEW_NEW
+            }
+        }
+
+        return reference to tested
+    }
+}
